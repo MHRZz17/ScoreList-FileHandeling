b29338338b226bec3b8073eee92ae5e8180873df
MHRZz17 <admmqtt190743@gmail.com>
2025-03-05 16:42:39 +0330

    base complete!need to fix bug

--- Tamrin1.py
+++ Tamrin1.py
@@ -1,45 +1,72 @@
-name_lst=[]
-score_lst=[]
-def inputName(name_lst):
-    name_lst.append(input("Enter a name : "))
-    return name_lst
-def inputScore(score_lst):
-    score_lst.append(input("Enter a score : "))
-    return score_lst
 
 def creatFile():
-    file_name = input("Enter a file name : ")
-    file_name+=".txt"
-    f=open(file_name,"x")
+    # file_name = input("Enter a file name : ")
+    # file_name+=".txt"
+    f=open("ScoreList.txt","x")
     f.close()
 def readFile():
-    f=open("Tamrin1.txt","r")
+    f=open("ScoreList.txt","r")
     list=f.read()
     f.close()
     return list
-def updateFile():
-    f=open("Tamrin1.txt","a")
-    list=f.write()
+def addNameScore(name,score):
+    f=open("ScoreList.txt","a")
+    f.write(f"{name},{score}\n")
     f.close()
-    return list
-def deleteFile():
+def editNameScore():
+    pass
+def deleteNameScore():
     pass
 def maxScore():
     pass
 def minScore():
     pass
-def averageScore():
-    pass
+def averageScore(score_lst):
+    sum=0
+    n=len(score_lst)
+    for i in score_lst:
+        sum+=i
+    avg=sum/n
+    return avg
 
-while():
-    print("1.addName\n2.addScore\n3.creatFile\n4.readFile\n5.updateFile\n6.deleteFile\n7.maxScore\n8.minScore\n9.averageScore\n0.exit")
+
+while True:
+    name_lst = []
+    score_lst = []
+    # list_dic = {}
+    # list = readFile()
+    # list_ = list.split("\n")
+    # for i in list_:
+    #     name_score = i.split(",")
+    #     name_lst.append(name_score[0])
+    #     score_lst.append(int(name_score[1]))
+    print("1.creatFile|2.readFile|3.addNameScore|4.editNameScore|5.deleteNameScore|6.maxScore|7.minScore|8.averageScore|0.exit")
     num=int(input("Enter Number:"))
     if num==1:
-        name_lst=inputName(name_lst)
+        try:
+            creatFile()
+            print("Secsesfull!")
+        except:
+            print("File was existed!")
     if num==2:
-        score_lst=inputScore(score_lst)
+        try:
+            list=readFile()
+            list_=list.split("\n")
+            for i in list_:
+                name_score=i.split(",")
+                print("name:",name_score[0],"|","score:",name_score[1])
+        except:
+            print("File not found or your file is empty!\nPlease creat file or add note to file!")
     if num==3:
-        pass
+        try:
+            name=input("Enter a name : ")
+            score=input("Enter a score : ")
+            name_lst.append(name)
+            score_lst.append(int(score))
+            addNameScore(name,score)
+            print("Successfully added!")
+        except:
+            print("Error")
     if num==4:
         pass
     if num==5:
@@ -49,9 +76,17 @@ while():
     if num==7:
         pass
     if num==8:
-        pass
-    if num==9:
-        pass
+        # try:
+        list = readFile()
+        list_ = list.split("\n")
+        for i in list_:
+            name_score = i.split(",")
+            score_lst.append(int(name_score[1]))
+
+        avg=averageScore(score_lst)
+        print("average score:",avg)
+        # except:
+        #     print("Error")
     if num==0:
         pass
 
